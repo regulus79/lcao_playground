fa803861884295e91d603f1f2fa0b3ca7c2cc917
regulus79 <117475203+regulus79@users.noreply.github.com>
2026-03-22 19:35:46 -0400

    More changed

--- xyz_view.py
+++ xyz_view.py
@@ -10,6 +10,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("inputfile")
 parser.add_argument("--orbitals", type=str, choices=["all", "valence", "pz"], required=True)
 parser.add_argument("--plot", type=str, choices=["all", "frontier"], required=True)
+parser.add_argument("--quantile", type=float, default=0.5)
+parser.add_argument("--buffer", type=float, default=3)
 args = parser.parse_args()
 print(f"Parsing file {args.inputfile}")
 
@@ -26,7 +28,7 @@ print("Valence charges:", valence_charges)
 
 molecule_extent = np.max(np.abs(atom_positions))
 print("Molecule max extent (m):", molecule_extent)
-molecule_extent_buffered = molecule_extent * 1.2 + 1 * bohr_radius
+molecule_extent_buffered = molecule_extent + args.buffer * bohr_radius
 print("Buffered plot radius (m):", molecule_extent_buffered)
 
 lattice_shape = (32, 32, 32)
@@ -37,15 +39,17 @@ for i in range(len(atom_positions)):
 	atom_positions[i] += np.array([0.5, 0.5, 0.5])*latticeLengthPerStep()
 
 orbital_funcs = []
-if args.orbitals == "all":
-	orbital_funcs = parse_xyz.default_orbital_functions(atom_charges)
-elif args.orbitals == "valence":
-	orbital_funcs = parse_xyz.valence_orbital_functions(atom_charges)
-elif args.orbitals == "pz":
-	orbital_funcs = parse_xyz.z_orbital_functions(atom_charges)
 atomic_orbitals = []
 total_potential = np.zeros(lattice_shape, dtype = complex)
-generate_orbitals(atom_positions, atom_charges, orbital_funcs, atomic_orbitals, total_potential)
+if args.orbitals == "all":
+	orbital_funcs = parse_xyz.default_orbital_functions(atom_charges)
+	generate_orbitals(atom_positions, atom_charges, orbital_funcs, atomic_orbitals, total_potential)
+elif args.orbitals == "valence":
+	orbital_funcs = parse_xyz.valence_orbital_functions(atom_charges)
+	generate_orbitals(atom_positions, valence_charges, orbital_funcs, atomic_orbitals, total_potential)
+elif args.orbitals == "pz":
+	orbital_funcs = parse_xyz.z_orbital_functions(atom_charges)
+	generate_orbitals(atom_positions, atom_charges, orbital_funcs, atomic_orbitals, total_potential) # TODO
 print(f"Generated {len(atomic_orbitals)} orbitals")
 
 eigs = calculateMOcoeffs(atomic_orbitals, total_potential)
@@ -54,6 +58,10 @@ print("Calculated MO coeffs")
 MOs = getMOs(atomic_orbitals, eigs)
 
 print("Eigenvalues (eV):", np.sort(eigs.eigenvalues) / charge_e)
+print(eigs)
+print("Eigenvectors:")
+for i in np.argsort(eigs.eigenvalues):
+	print(np.round(eigs.eigenvectors[:, i], 4))
 
 energies = np.real(np.sort(eigs.eigenvalues))
 #totalEnergy = groundStateEnergy(energies, atom_positions, atom_charges)
@@ -70,12 +78,12 @@ print(f"HOMO-LUMO Gap: {homo_lumo_gap_energy / charge_e} eV")
 
 if args.plot == "all":
 	if args.orbitals == "valence":
-		plotMOs(atomic_orbitals, eigs, atom_positions, valence_charges, quantile=0.5, num_cols=4)
+		plotMOs(atomic_orbitals, eigs, atom_positions, valence_charges, quantile=args.quantile, num_cols=4)
 	else:
-		plotMOs(atomic_orbitals, eigs, atom_positions, atom_charges, quantile=0.5, num_cols=4) # todo what about pz
+		plotMOs(atomic_orbitals, eigs, atom_positions, atom_charges, quantile=args.quantile, num_cols=4) # todo what about pz
 elif args.plot == "frontier":
-	#plotHOMOLUMO(atomic_orbitals, eigs, valence_charges, quantile=0.5)
+	#plotHOMOLUMO(atomic_orbitals, eigs, valence_charges, quantile=args.quantile)
 	if args.orbitals == "valence":
-		plotHOMOLUMO_more(atomic_orbitals, eigs, atom_positions, valence_charges, quantile=0.5)
+		plotHOMOLUMO_more(atomic_orbitals, eigs, atom_positions, valence_charges, quantile=args.quantile)
 	else:
-		plotHOMOLUMO_more(atomic_orbitals, eigs, atom_positions, atom_charges, quantile=0.5)
+		plotHOMOLUMO_more(atomic_orbitals, eigs, atom_positions, atom_charges, quantile=args.quantile)
